ea55267656b1fba6d9ffadd697ccf80c2b349d16
Asus <Asus@DESKTOP-JN4R0B2>
2018-08-28 00:59:15 +0700

    PBO II Latihan 1 .1

--- src/pasien/MainTest.java
+++ src/pasien/MainTest.java
@@ -5,6 +5,9 @@
  */
 package pasien;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author Asus
@@ -13,17 +16,23 @@ public class MainTest {
     public static void main(String[] args) {
         
         Pasien pas1 = new Pasien("Prama");
+        Date date = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("yyyyMMdd");
+        String noRekamMedis = ft.format(date) + pas1.getNama().substring(0, 3);
 
         try {
         pas1.setTanggalLahir(22);
         pas1.setBulanLahir(4);
-        pas1.setTahunLahir(1999);  
+        pas1.setTahunLahir(1999);
+        pas1.setNoRekamMedis(noRekamMedis);
         }
         
         catch (Exception ex) {
             ex.printStackTrace();
         }
         System.out.println(pas1.getNama());
-        System.out.println(pas1.getTanggalLahir()+"-"+pas1.getBulanLahir()+"-"+pas1.getTahunLahir());     
+        System.out.println(pas1.getTanggalLahir()+"-"+pas1.getBulanLahir()+"-"+pas1.getTahunLahir());
+        System.out.println(pas1.getNoRekamMedis());
+        
     }
 }
