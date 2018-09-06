18918a0de1c7be24c77a44059afd1af62909fb43
Asus <Asus@DESKTOP-JN4R0B2>
2018-09-06 11:50:02 +0700

    PBO II Latihan 1.2

--- src/Model/AntrianPasien.java
+++ src/Model/AntrianPasien.java
@@ -5,61 +5,61 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jarkom
  */
 public class AntrianPasien {
-
+    
     private int tanggalAntrian;
     private int bulanAntrian;
     private int tahunAntrian;
-    private int nomorAntrian = 0;
-    private final int JUMLAH_MAKSIMAL_PASIEN = 50;
     private Klinik klinik;
-    private Pasien[] daftarPasien = new Pasien[JUMLAH_MAKSIMAL_PASIEN];
-
+    private ArrayList<Pasien> daftarPasien = new ArrayList<Pasien>();
+    
     public AntrianPasien() {
-
+        
     }
-
+    
     public int getTanggalAntrian() {
         return tanggalAntrian;
     }
-
+    
     public void setTanggalAntrian(int tanggalAntrian) {
         this.tanggalAntrian = tanggalAntrian;
     }
-
+    
     public int getBulanAntrian() {
         return bulanAntrian;
     }
-
+    
     public void setBulanAntrian(int bulanAntrian) {
         this.bulanAntrian = bulanAntrian;
     }
-
+    
     public int getTahunAntrian() {
         return tahunAntrian;
     }
-
+    
     public void setTahunAntrian(int tahunAntrian) {
         this.tahunAntrian = tahunAntrian;
     }
-
+    
     public Klinik getKlinik() {
         return klinik;
     }
-
+    
     public void setKlinik(Klinik klinik) {
         this.klinik = klinik;
     }
-
-    public Pasien[] getDaftarPasien() {
+    
+    public ArrayList<Pasien> getDaftarPasien() {
         return daftarPasien;
     }
-
-    public void setDaftarPasien(Pasien[] daftarPasien) {
+    
+    public void setDaftarPasien(ArrayList<Pasien> daftarPasien) {
         this.daftarPasien = daftarPasien;
     }
 
@@ -69,26 +69,8 @@ public class AntrianPasien {
      * @param pasien
      * @throws Exception
      */
-    public void Mendaftar(Pasien pasien) throws Exception {
-        // nomor antrian dicek dengan jumlah maksimal pasien yang dilayani
-        if (nomorAntrian < JUMLAH_MAKSIMAL_PASIEN) {
-            daftarPasien[nomorAntrian] = pasien;
-            nomorAntrian++;
-        } else {
-            // antrian sudah penuh
-            throw new Exception("Antrian Penuh Bos");
-        }
+    public void Mendaftar(Pasien pasien) {
+        daftarPasien.add(pasien);
     }
-
-    /**
-     * fungsi ini digunakan untuk memanggil nomor antrian pasien oeh dokter
-     *
-     * @param nomorAntrian
-     * @return
-     */
-    public Pasien panggilPasien(int nomorAntrian) {
-        return daftarPasien[nomorAntrian];
-
-    }
-
+    
 }
