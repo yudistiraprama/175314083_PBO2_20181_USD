4b40ea1b089df8a88bf35844e66c4d48edd4345e
Asus <Asus@DESKTOP-JN4R0B2>
2018-09-06 12:03:04 +0700

    PBO II Latihan 1.2

--- src/Model/Klinik.java
+++ src/Model/Klinik.java
@@ -10,32 +10,69 @@ package Model;
  * @author jarkom
  */
 public class Klinik {
-    
+
     private String idKlinik;
     private String nama;
-    
-    public Klinik(){
-        
+
+    /**
+     * constructor untuk mendeklarasikan objek KLinik
+     */
+    public Klinik() {
+
     }
-    
-    public Klinik (String idKlinik, String nama){
+
+    /**
+     * constructor untuk mendeklarasikan objek KLinik dengan parameter lokal
+     * idKlinik dan nama dengan tipe data String
+     *
+     * @param nama
+     */
+    public Klinik(String idKlinik, String nama) {
+        // pernyataan bahwa nilain variabel idKlinik sama dengan nilai dari variabel lokal idKlinik
         this.idKlinik = idKlinik;
+        // pernyataan bahwa nilain variabel nama sama dengan nilai dari variabel lokal nama
         this.nama = nama;
     }
 
+    /**
+     * method untuk mengambil nilai dari variabel idKlinik
+     *
+     * @return
+     */
     public String getIdKlinik() {
+        //pengambalian nilai dari variabel idKlinik
         return idKlinik;
     }
 
+    /**
+     * method untuk meng-set nilai dari variabel idKLinik dengan variabel lokal
+     * idKLinik dengan tipe data String
+     *
+     * @param nama
+     */
     public void setIdKlinik(String idKlinik) {
+        // pernyataan bahwa nilai dari variabel idKlinik sama dengan nilai dari variabel lokal idKLinik
         this.idKlinik = idKlinik;
     }
 
+    /**
+     * method untuk mengambil nilai dari variabel nama
+     *
+     * @return
+     */
     public String getNama() {
+        //pengambalian nilai dari variabel nama
         return nama;
     }
 
+    /**
+     * method untuk meng-set nilai dari variabel nama dengan variabel lokal nama
+     * dengan tipe data String
+     *
+     * @param nama
+     */
     public void setNama(String nama) {
+        // pernyataan bahwa nilai dari variabel nama sama dengan nilai dari variabel lokal nama
         this.nama = nama;
     }
 }
