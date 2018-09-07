5eefa79128f4056d4d10d07b1d6020a8a74eff5a
jarkom <jarkom@jarkom-C00>
2018-09-07 12:51:14 +0700

    PBO II 1.3

--- src/Test/MainTest.java
+++ src/Test/MainTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package Test;
 
+import Model.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
