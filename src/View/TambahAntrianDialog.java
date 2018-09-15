d53b1426bce7dbe7d355e3f01b1fb0b4f0998b03
Asus <Asus@DESKTOP-JN4R0B2>
2018-09-16 00:49:37 +0700

    PBO II Latihan 1.5

--- src/View/TambahAntrianDialog.java
+++ src/View/TambahAntrianDialog.java
@@ -20,7 +20,7 @@ import javax.swing.JTextField;
  * @author jarkom
  */
 public class TambahAntrianDialog extends JDialog implements ActionListener {
-    
+
     private JLabel judulLabel;
     private JLabel namaLabel;
     private JLabel noRMLabel;
@@ -28,79 +28,79 @@ public class TambahAntrianDialog extends JDialog implements ActionListener {
     private JTextField namaText;
     private JTextField noRMText;
     private JTextField alamatText;
-    private JButton tambahButton;
-    
+    private JButton antriButton;
+
     public TambahAntrianDialog() {
         init();
     }
-    
+
     public TambahAntrianDialog(String title) {
         this.setTitle(title);
         init();
     }
-    
+
     public void init() {
         this.setLayout(null);
-        
+
         judulLabel = new JLabel();
         judulLabel.setText("FORM DAFTAR ANTRIAN");
         judulLabel.setFont(new Font(null, Font.BOLD, 16));
         judulLabel.setBounds(50, 20, 250, 30);
         this.add(judulLabel);
-        
+
         noRMLabel = new JLabel();
         noRMLabel.setText("No. RM");
         noRMLabel.setBounds(50, 70, 80, 20);
         this.add(noRMLabel);
-        
+
         noRMText = new JTextField();
         noRMText.setBounds(150, 70, 120, 20);
         this.add(noRMText);
-        
+
         namaLabel = new JLabel();
         namaLabel.setText("Nama");
         namaLabel.setBounds(50, 100, 80, 20);
         this.add(namaLabel);
-        
+
         namaText = new JTextField();
         namaText.setBounds(150, 100, 120, 20);
         this.add(namaText);
-        
+
         alamatLabel = new JLabel();
         alamatLabel.setText("Alamat");
         alamatLabel.setBounds(50, 130, 80, 20);
         this.add(alamatLabel);
-        
+
         alamatText = new JTextField();
         alamatText.setBounds(150, 130, 120, 20);
         this.add(alamatText);
-        
-        tambahButton = new JButton();
-        tambahButton.setText("Antri");
-        tambahButton.setBounds(150, 180, 80, 30);
-        this.add(tambahButton);
-        
+
+        antriButton = new JButton();
+        antriButton.setText("Antri");
+        antriButton.setBounds(150, 180, 80, 30);
+        this.add(antriButton);
+ 
         noRMText.addActionListener(this);
-        tambahButton.addActionListener(this);
-        
+        antriButton.addActionListener(this);
+
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == noRMText) {
             Pasien cari = Pasien.cariPasien(noRMText.getText());
             if (cari == null) {
-                JOptionPane.showMessageDialog(null, "Data Pasien"+noRMText.getText() +"Tidak Ada");
+                JOptionPane.showMessageDialog(null, "Data Pasien " + noRMText.getText() + " Tidak Ada");
             } else {
                 namaText.setText(cari.getNama());
                 alamatText.setText(cari.getAlamat());
             }
         }
-        if (ae.getSource() == tambahButton) {
+        if (ae.getSource() == antriButton) {
             Pasien cari = Pasien.cariPasien(noRMText.getText());
             for (int i = 0; i < Pasien.daftarPasien.size(); i++) {
                 if (cari == Pasien.daftarPasien.get(i)) {
-                    JOptionPane.showMessageDialog(null, "Anda Telah Mengantri");
+                    JOptionPane.showMessageDialog(null, "Nomor Antrian Anda : " + (i + 1));
                 }
             }
         }
